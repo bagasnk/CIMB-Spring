238253532db520c0eb0d0d0f921a416d2f182152
Bagas Yafitra Pandji <34235801+bagasnk@users.noreply.github.com>
2020-06-12 20:19:58 +0700

    add EmployeeAddress by ID

--- tokolapak/src/main/java/com/cimb/tokolapak/service/EmployeeService.java
+++ tokolapak/src/main/java/com/cimb/tokolapak/service/EmployeeService.java
@@ -1,7 +1,11 @@
 package com.cimb.tokolapak.service;
+import java.util.Optional;
+import com.cimb.tokolapak.entity.Employee;
 import com.cimb.tokolapak.entity.EmployeeAddress;
 
 public interface EmployeeService {
 	
 	public void deleteEmployeeAddress(EmployeeAddress employeeAddress); 
+	public Optional<Employee> addEmployeeAddress(int id,  EmployeeAddress employeeAddress);
+	
 }
--- tokolapak/src/main/java/com/cimb/tokolapak/service/impl/EmployeeServiceImpl.java
+++ tokolapak/src/main/java/com/cimb/tokolapak/service/impl/EmployeeServiceImpl.java
@@ -1,10 +1,16 @@
 package com.cimb.tokolapak.service.impl;
 
+
+import java.util.Optional;
+
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.cimb.tokolapak.dao.EmployeeAddressRepo;
 import com.cimb.tokolapak.dao.EmployeeRepo;
+import com.cimb.tokolapak.entity.Employee;
 import com.cimb.tokolapak.entity.EmployeeAddress;
 import com.cimb.tokolapak.service.EmployeeService;
 
@@ -18,11 +24,24 @@ public class EmployeeServiceImpl implements EmployeeService {
 	private EmployeeAddressRepo employeeAddressRepo;
 
 	@Override
+	@Transactional
 	public void deleteEmployeeAddress(EmployeeAddress employeeAddress) {
 		employeeAddress.getEmployee().setEmployeeAddress(null); // putus hubungan dari employe ke address
 		employeeAddress.setEmployee(null);	// putushubungan dari addres sampai employee
-		
 		employeeAddressRepo.delete(employeeAddress);
 	}
-
+	
+	@Override
+	@Transactional
+	public Optional<Employee> addEmployeeAddress(int id, EmployeeAddress employeeAddress){
+		Optional<Employee> findEmployee = employeeRepo.findById(id);
+		
+		employeeAddress.setEmployee(findEmployee.get());
+		employeeAddressRepo.save(employeeAddress);
+		employeeAddress.getEmployee().setEmployeeAddress(employeeAddress);
+		employeeRepo.save(findEmployee.get());
+		
+		return employeeRepo.findById(id);
+	}
+	
 }
